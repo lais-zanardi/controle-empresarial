9e5843e66bdd7874a0bb9deaae08b16b5b882fb8
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-01-07 11:52:43 -0300

    Initial commit

---
+++ src/test/java/br/com/ada/controle/empresarial/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.controle.empresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
